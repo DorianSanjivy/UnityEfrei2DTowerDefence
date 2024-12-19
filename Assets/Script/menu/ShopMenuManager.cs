46d672de9b0d26014ece3a3eca1f05ff8e22876e
DorianSanjivy <78807938+DorianSanjivy@users.noreply.github.com>
2024-12-19 15:58:37 +0100

    recreate menu

--- Assets/Script/menu/ShopMenuManager.cs
+++ Assets/Script/menu/ShopMenuManager.cs
@@ -7,10 +7,13 @@ public class ShopMenuManager : MonoBehaviour
     public GameObject shopPanel; // Le panel du menu
     public GameObject shopButton;
 
+    public bool activate = false;
+
     public void ToggleShop()
     {
         shopPanel.SetActive(!shopPanel.activeSelf);
         shopButton.SetActive(!shopButton.activeSelf);
+        activate = !activate;
     }
 }
 
