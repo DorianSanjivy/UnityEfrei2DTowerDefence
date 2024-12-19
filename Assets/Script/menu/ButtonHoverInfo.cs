54c8081f2707713e582dea179572847f1819aa7e
Damien Lin <79983884+Corpsy667@users.noreply.github.com>
2024-12-19 16:52:03 +0100

    Description update

--- Assets/Script/menu/ButtonHoverInfo.cs
+++ Assets/Script/menu/ButtonHoverInfo.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
@@ -77,5 +78,23 @@ public class ButtonHoverInfo : MonoBehaviour
         {
             infoText.text = string.Empty;
         }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+>>>>>>> Stashed changes
     }
 }
